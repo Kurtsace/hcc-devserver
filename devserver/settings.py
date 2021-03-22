2f0893ebee59543d62bac809d817a0c151330a69
kurtsace <palokp@myumanitoba.ca>
2021-03-22 16:51:52 -0500

    Switched to postgres DB, updated requirements

--- devserver/settings.py
+++ devserver/settings.py
@@ -92,20 +92,13 @@ WSGI_APPLICATION = 'devserver.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-'''
-'default': {
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': data['NAME'],
         'USER': data['USER'],
         'PASSWORD': data['PASSWORD'],
         'HOST': data['HOST'],
-    }
-'''
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
